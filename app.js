db24a8057f9beccf33f5e09475bc2c0269f848c2
Jeff Lovell <jeff@lovellworks.com>
2020-06-18 02:31:35 +0000

    add PORT to bind to

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var PORT = process.env.PORT || 5000
 
 app.set("view engine", "ejs");
 
@@ -11,6 +12,6 @@ app.get("/about", function (req, res){
 	res.render("about");
 });
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
 	console.log("Server Running on Port 3000")
 });
